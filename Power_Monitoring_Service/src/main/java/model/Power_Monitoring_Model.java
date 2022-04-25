28446e4f04c808cd764a7d60baf4001b2e8bc6e6
nisal <nisal@Nisal>
2022-04-25 19:51:30 +0530

    Final commit

--- Power_Monitoring_Service/src/main/java/model/Power_Monitoring_Model.java
+++ Power_Monitoring_Service/src/main/java/model/Power_Monitoring_Model.java
@@ -20,7 +20,7 @@ public class Power_Monitoring_Model
 	 } 
 	
 	
-	public String insertData(int id, String name, String eType, String cMW, String status) 
+	public String insertData(String name, String eType, String cMW, String status) 
 	 { 
 			 String output = ""; 
 			 try
@@ -30,14 +30,14 @@ public class Power_Monitoring_Model
 				 {return "Error while connecting to the database for inserting."; } 
 				 
 				 String query = " insert into power_station_data(`PS_id`,`PS_name`,`PS_eType`,`PS_cMW`,`PS_status`)"
-				 + " values (?, ?, ?, ?, ?)"; 
+				 + " values (null, ?, ?, ?, ?)"; 
 				 PreparedStatement preparedStmt = con.prepareStatement(query); 
 				
-				 preparedStmt.setInt(1, 0); 
-				 preparedStmt.setString(2, name); 
-				 preparedStmt.setString(3, eType); 
-				 preparedStmt.setString(4, cMW); 
-				 preparedStmt.setString(5, status); 
+				 
+				 preparedStmt.setString(1, name); 
+				 preparedStmt.setString(2, eType); 
+				 preparedStmt.setString(3, cMW); 
+				 preparedStmt.setString(4, status); 
 				
 
 				 preparedStmt.execute(); 
@@ -103,7 +103,64 @@ public class Power_Monitoring_Model
 	 } 
 	 return output; 
 	 } 
-	public String updateData(String id, String status, String cMW)
+	
+	 // /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+	public String readDatabyid(String ids) 
+	 { 
+			String output = ""; 
+			 try
+			 { 
+				 Connection con = connect(); 
+				 if (con == null) 
+				 {return "Error while connecting to the database for reading."; } 
+				 
+				 output = "<table border='1'><tr><th>P.S. Name</th><th>Energy Type</th>" +
+						 "<th>Mega Watts</th>" + 
+						 "<th>Station Status</th>" +
+						 "<th>Update</th><th>Delete</th></tr>"; 
+				 
+				 String query = "select * from power_station_data where PS_id='"+ids+"'"; 
+				 Statement stmt = con.createStatement(); 
+				 ResultSet rs = stmt.executeQuery(query); 
+				 
+				
+				 while (rs.next()) 
+				 { 
+						 String id = Integer.toString(rs.getInt("PS_id")); 
+						 String name = rs.getString("PS_name"); 
+						 String eType = rs.getString("PS_eType"); 
+						 String cMW = rs.getString("PS_cMW");  
+						 String status = rs.getString("PS_status"); 
+						 
+						
+						 output += "<tr><td>" + name + "</td>"; 
+						 output += "<td>" + eType + "</td>"; 
+						 output += "<td>" + cMW + "</td>"; 
+						 output += "<td>" + status + "</td>"; 
+						 
+						 // Remember add power_monitoring.jsp here
+						 
+						 output += "<td><input name='btnUpdate' type='button' value='Update' class='btn btn-secondary'></td>"		
+									 + "<td><form method='post' action='Power_Monitoring.jsp'>"
+									 + "<input name='btnRemove' type='submit' value='Remove' class='btn btn-danger'>"		 		
+									 + "<input name='id' type='hidden' value='" + id 
+									 + "'>" + "</form></td></tr>"; 
+				 } 
+				 con.close(); 
+				
+				 output += "</table>"; 
+	 } 
+	 catch (Exception e) 
+	 { 
+				 output = "Error while reading Data."; 
+				 System.err.println(e.getMessage()); 
+	 } 
+	 return output; 
+	 } 
+	
+	////////////////////////////////////////////////UPDATE///////////////////////////////////////////////////
+	
+	public String updateData(String PS_id,String PS_name, String PS_eType,String PS_cMW, String PS_status )
 	
 	{ 
 		 String output = ""; 
@@ -114,14 +171,18 @@ public class Power_Monitoring_Model
 				 if (con == null) 
 				 {return "Error while connecting to the database for updating."; } 
 				 
-				 String query = "UPDATE power_station_data SET PS_cMW=? PS_status=? WHERE PS_id=?"; 
+				 String query = "UPDATE power_station_data SET PS_name=?, PS_eType=?, PS_cMW=?, PS_status=? WHERE PS_id=?"; 
 				 
 				 PreparedStatement preparedStmt = con.prepareStatement(query); 
 				 
 				 
-				 preparedStmt.setString(1, cMW); 
-				 preparedStmt.setString(2, status); 
-				 preparedStmt.setInt(3, Integer.parseInt(id)); 
+				 preparedStmt.setString(1, PS_name); 
+				 preparedStmt.setString(2, PS_eType);
+				 preparedStmt.setString(3, PS_cMW);
+				 preparedStmt.setString(4, PS_status); 
+				 preparedStmt.setInt(5, Integer.parseInt(PS_id));
+				 
+				 
 				 
 				 
 				 preparedStmt.execute(); 
@@ -137,8 +198,9 @@ public class Power_Monitoring_Model
 		 return output; 
 		 } 
 	
+	///////////////////////////DELETE//////////////////////////////////////////////
 	
-		public String deleteRow(String id) 
+		public String deleteRow(String PS_id) 
 		 { 
 			
 		 String output = ""; 
@@ -153,7 +215,7 @@ public class Power_Monitoring_Model
 				 PreparedStatement preparedStmt = con.prepareStatement(query); 
 				 // binding values
 				 
-				 preparedStmt.setInt(1, Integer.parseInt(id)); 
+				 preparedStmt.setInt(1, Integer.parseInt(PS_id)); 
 				 // execute the statement
 				 
 				 preparedStmt.execute(); 
